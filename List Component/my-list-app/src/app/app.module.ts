fa9f51bf25e8f99f319d7ff02588bbe243125a07
TashevYanislav <tashevyanislav@gmail.com>
2024-03-06 20:07:29 +0200

    Promise and Observable exercise

--- List Component/my-list-app/src/app/app.module.ts
+++ List Component/my-list-app/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { UserListComponent } from './user-list/user-list.component';
 import { UserItemComponent } from './user-item/user-item.component';
+import { Observable } from 'rxjs';
 
 @NgModule({
   declarations: [
@@ -18,3 +19,19 @@ import { UserItemComponent } from './user-item/user-item.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+Promise.resolve(100).then(data=>data*40).then((data)=>console.log(data));
+
+
+const o = new Observable((observer)=>{
+  observer.next(100)
+  observer.next(200)
+  observer.next(300)
+  observer.next(400)
+})
+
+o.subscribe((data)=>{
+  console.log("from observable",data);
+  
+})
