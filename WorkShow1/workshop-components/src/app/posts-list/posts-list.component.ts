ee98ecd91a824445b1e542b845abdf55053b092b
TashevYanislav <tashevyanislav@gmail.com>
2024-03-11 16:51:07 +0200

    implemented dynamic rendering

--- WorkShow1/workshop-components/src/app/posts-list/posts-list.component.ts
+++ WorkShow1/workshop-components/src/app/posts-list/posts-list.component.ts
@@ -1,18 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
+import { Post } from '../types/post';
 
 @Component({
   selector: 'app-posts-list',
   templateUrl: './posts-list.component.html',
   styleUrls: ['./posts-list.component.css'],
 })
-export class PostsListComponent implements OnInit{
+export class PostsListComponent implements OnInit {
+  posts: Post[] = [];
   constructor(private api: ApiService) {}
 
-ngOnInit(): void {
-  this.api.getPosts().subscribe(posts=>{
-    console.log(posts);
-    
-  })
-}
+  ngOnInit(): void {
+    this.api.getPosts(5).subscribe((posts) => {
+      this.posts = posts;
+    });
+  }
 }
