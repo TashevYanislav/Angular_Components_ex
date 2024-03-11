6144bab1ed5a19715ad337e6edaa2b85c69dd05b
TashevYanislav <tashevyanislav@gmail.com>
2024-03-11 15:46:42 +0200

    Http Get functionality added

--- WorkShow1/workshop-components/src/app/app.module.ts
+++ WorkShow1/workshop-components/src/app/app.module.ts
@@ -8,10 +8,22 @@ import { SharedModule } from './shared/shared.module';
 import { MainComponent } from './main/main.component';
 import { ThemesListComponent } from './themes-list/themes-list.component';
 import { PostsListComponent } from './posts-list/posts-list.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [AppComponent, MainComponent, ThemesListComponent, PostsListComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule, SharedModule],
+  declarations: [
+    AppComponent,
+    MainComponent,
+    ThemesListComponent,
+    PostsListComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule,
+    SharedModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
