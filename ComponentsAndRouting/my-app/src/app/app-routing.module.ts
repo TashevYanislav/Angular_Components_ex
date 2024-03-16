534db1d19fe5a6dd0ae10ab8adc079b84a65d4a4
TashevYanislav <tashevyanislav@gmail.com>
2024-03-16 18:08:23 +0200

    Add Routing

--- ComponentsAndRouting/my-app/src/app/app-routing.module.ts
+++ ComponentsAndRouting/my-app/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserListComponent } from './user/user-list/user-list.component';
+import { ToDoListComponent } from './to-do/to-do-list/to-do-list.component';
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ToDoListComponent },
+  { path: 'user-list', component: UserListComponent },
+  { path: 'todo-list', component: ToDoListComponent },
+  { path: 'error-page', component: ErrorPageComponent },
+  { path: '**', redirectTo: 'error-page' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
