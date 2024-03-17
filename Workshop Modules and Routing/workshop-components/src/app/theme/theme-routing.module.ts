06504d0259a7d18636b664b20623043535aeaa31
TashevYanislav <tashevyanislav@gmail.com>
2024-03-18 00:59:40 +0200

    Put all Theme releted components in Theme module

--- Workshop Modules and Routing/workshop-components/src/app/theme/theme-routing.module.ts
+++ Workshop Modules and Routing/workshop-components/src/app/theme/theme-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from '../home/home.component';
-import { MainComponent } from '../main/main.component';
+import { MainComponent } from './main/main.component';
 import { AddThemeComponent } from './add-theme/add-theme.component';
 
 const routes: Routes = [
