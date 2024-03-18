3cae4844e4228d43d74ef272d6827f5b0f0b1380
TashevYanislav <tashevyanislav@gmail.com>
2024-03-18 20:04:09 +0200

    Login funtionality

--- Workshop Modules and Routing/workshop-components/src/app/types/user.ts
+++ Workshop Modules and Routing/workshop-components/src/app/types/user.ts
@@ -1,13 +1,19 @@
-export interface User{
-    
-        themes: string[];
-        posts: string[];
-        _id: string;
-        tel: string;
-        email: string;
-        username: string;
-        password: string;
-        created_at: string;
-        updatedAt: string;
-        __v: number;
+export interface User {
+  themes: string[];
+  posts: string[];
+  _id: string;
+  tel: string;
+  email: string;
+  username: string;
+  password: string;
+  created_at: string;
+  updatedAt: string;
+  __v: number;
+}
+
+export interface AuthUser {
+  firstName: string;
+  email: string;
+  phoneNumber: string;
+  password: string;
 }
--- Workshop Modules and Routing/workshop-components/src/app/user/user.service.ts
+++ Workshop Modules and Routing/workshop-components/src/app/user/user.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { AuthUser } from '../types/user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  USER_KEY = '[user]';
+  user: AuthUser | undefined;
 
-  constructor() { }
+  get isLogged(): boolean {
+    return !!this.user;
+  }
+
+  constructor() {
+    try {
+      const LsUser = localStorage.getItem(this.USER_KEY) || '';
+      this.user = JSON.parse(LsUser);
+    } catch (error) {
+      console.log(error);
+      this.user = undefined;
+    }
+  }
+
+  login() {
+    this.user = {
+      firstName: 'Petko',
+      email: 'petko@abv.bg',
+      password: '123123',
+      phoneNumber: '088-888-8888',
+    };
+    localStorage.setItem(this.USER_KEY, JSON.stringify(this.user));
+  }
+  logout() {
+    this.user = undefined;
+    localStorage.removeItem(this.USER_KEY);
+  }
 }
