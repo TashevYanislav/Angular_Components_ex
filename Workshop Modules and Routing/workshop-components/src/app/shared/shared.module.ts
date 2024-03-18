0f41f9cef89141b2780fc3d4f1719a3bf4cf81c7
TashevYanislav <tashevyanislav@gmail.com>
2024-03-18 20:03:05 +0200

    add Welcome compoenent

--- Workshop Modules and Routing/workshop-components/src/app/shared/shared.module.ts
+++ Workshop Modules and Routing/workshop-components/src/app/shared/shared.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoaderComponent } from './loader/loader.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [
-    LoaderComponent
+    LoaderComponent,
+    WelcomeComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,RouterModule
   ],
-  exports:[LoaderComponent]
+  exports:[LoaderComponent,WelcomeComponent]
 })
 export class SharedModule { }
