27bbe7645e94a163d37344c52dbbafb4d4cb2ce1
TashevYanislav <tashevyanislav@gmail.com>
2024-03-19 18:07:25 +0200

    Add new Theme auth guard

--- Workshop Modules and Routing/workshop-components/src/app/theme/theme-routing.module.ts
+++ Workshop Modules and Routing/workshop-components/src/app/theme/theme-routing.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from '../home/home.component';
 import { MainComponent } from './main/main.component';
 import { AddThemeComponent } from './add-theme/add-theme.component';
 import { CurrentThemeComponent } from './current-theme/current-theme.component';
-
+import { AuthActivate } from '../guards/auth.activate';
 const routes: Routes = [
   {
     path: 'themes',
@@ -16,6 +15,7 @@ const routes: Routes = [
   {
     path: 'new-theme',
     component: AddThemeComponent,
+    canActivate: [AuthActivate],
   },
 ];
 
