b03d970fb4dc27610a3b24387a8773d482045fdd
TashevYanislav <tashevyanislav@gmail.com>
2024-03-19 18:05:15 +0200

    Add add theme component

--- Workshop Modules and Routing/workshop-components/src/app/theme/add-theme/add-theme.component.ts
+++ Workshop Modules and Routing/workshop-components/src/app/theme/add-theme/add-theme.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/api.service';
 
 @Component({
   selector: 'app-add-theme',
   templateUrl: './add-theme.component.html',
-  styleUrls: ['./add-theme.component.css']
+  styleUrls: ['./add-theme.component.css'],
 })
 export class AddThemeComponent {
+  constructor(private apiService: ApiService) {}
 
+  addTheme(e: Event, themeName: string, postText: string) {
+    e.preventDefault();
+    this.apiService
+      .createTheme(themeName, postText)
+      .subscribe((data) => console.log(data));
+  }
 }
