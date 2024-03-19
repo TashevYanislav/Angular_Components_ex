9566b82e3ebe0d6fcbd5d834cb990c80c7386bf1
TashevYanislav <tashevyanislav@gmail.com>
2024-03-19 18:03:50 +0200

    Update auth.activate.ts

--- Workshop Modules and Routing/workshop-components/src/app/guards/auth.activate.ts
+++ Workshop Modules and Routing/workshop-components/src/app/guards/auth.activate.ts
@@ -1,1 +1,30 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
   CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { UserService } from '../user/user.service';
+
+@Injectable({ providedIn: 'root' })
+export class AuthActivate implements CanActivate {
+  constructor(private userService: UserService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | boolean
+    | UrlTree
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree> {
+    if (this.userService.isLogged) {
+      return true;
+    } else {
+      return this.router.parseUrl('/login');
+    }
+  }
+}
