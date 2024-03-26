11ed0e117af05839898378e6ff753af1c8a91da8
TashevYanislav <tashevyanislav@gmail.com>
2024-03-26 15:30:13 +0200

    Hilight ex

--- Forms/directives_and_forms/src/app/highlight.directive.ts
+++ Forms/directives_and_forms/src/app/highlight.directive.ts
@@ -1,10 +1,32 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
-  selector: '[appHighlight]'
+  selector: '[appHighlight]',
 })
-export class HighlightDirective {
+export class HighlightDirective implements OnInit {
+  constructor(private elRef: ElementRef, private renderer: Renderer2) {}
 
-  constructor() { }
+  ngOnInit(): void {
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background', 'red');
 
+    this.renderer.listen(
+      this.elRef.nativeElement,
+      'mouseenter',
+      this.mouseEnteerHandler.bind(this)
+    );
+    this.renderer.listen(
+      this.elRef.nativeElement,
+      'mouseleave',
+      this.mouseLeaverHandler.bind(this)
+    );
+  }
+
+  mouseEnteerHandler(e: MouseEvent): void {
+    console.log(e);
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'red');
+  }
+  mouseLeaverHandler(e: MouseEvent): void {
+    console.log(e);
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'white');
+  }
 }
