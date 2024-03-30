d5eb52f17214569d39eb460f620ec4495b88f838
TashevYanislav <tashevyanislav@gmail.com>
2024-03-30 19:42:58 +0200

    Register func implemented

--- Workshop Modules and Routing/workshop-components/src/app/user/register/register.component.ts
+++ Workshop Modules and Routing/workshop-components/src/app/user/register/register.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
+  form = this.fb.group({
+    username:['gosho'],
+    email:['gosho@gmail.com'],
+    tel:['123-123-123']
+  });
+  constructor(private fb: FormBuilder) {}
 
+  register(): void {
+    if (this.form.invalid) {
+      return;
+    }
+    console.log(this.form.value);
+  }
 }
